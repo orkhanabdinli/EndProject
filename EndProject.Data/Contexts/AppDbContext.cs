87c8f4cbd8f5d01cdd1226fba621bb2a23ceb906
unknown <orkhansab@code.edu.az>
2024-05-19 18:14:18 +0400

    DbContext Created

---
+++ EndProject.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EndProject.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProject.Data.Contexts;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public DbSet<AppUser> Users {  get; set; }
+}
