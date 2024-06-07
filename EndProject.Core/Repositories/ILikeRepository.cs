6693cea6801864a6c999fe9eced0e4225296991f
unknown <orkhansab@code.edu.az>
2024-06-07 16:38:57 +0400

    ICommentRepository And ILikeRepository Created

---
+++ EndProject.Core/Repositories/ILikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndProject.Core.Repositories;
+
+public interface ILikeRepository : IGenericRepository<Like>
+{
+}
