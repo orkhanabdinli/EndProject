f41420709670d81f96c6dea2f99b5dc0b7e1622f
unknown <orkhansab@code.edu.az>
2024-06-07 16:36:29 +0400

    IAppUserRepository Created

---
+++ EndProject.Core/Repositories/IAppUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using EndProject.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EndProject.Core.Repositories;
+
+public interface IAppUserRepository
+{
+    DbSet<AppUser> Table { get; }
+    Task InsertAsync(AppUser user);
+    Task<int> CommitAsync();
+    Task<List<AppUser>> GetAllAsync(Expression<Func<AppUser, bool>> expression = null, params string[] includes);
+    Task<AppUser> GetSingleAsync(Expression<Func<AppUser, bool>> expression = null, params string[] includes);
+    Task<AppUser> GetByIdAsync(int id);
+    Task<bool> IsExist(Expression<Func<AppUser, bool>> expression);
+    void Delete(AppUser user);
+}
