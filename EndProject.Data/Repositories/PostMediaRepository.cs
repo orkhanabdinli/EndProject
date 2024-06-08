3c6f5f6a447b0f8ad0ce9ddb354196bc13613652
unknown <orkhansab@code.edu.az>
2024-06-08 20:11:01 +0400

    PostMediaRepository Created

---
+++ EndProject.Data/Repositories/PostMediaRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+
+namespace EndProject.Data.Repositories;
+
+public class PostMediaRepository : GenericRepository<PostMedia>, IPostMediaRepository
+{
+    public PostMediaRepository(AppDbContext context) : base(context)
+    {
+    }
+}
