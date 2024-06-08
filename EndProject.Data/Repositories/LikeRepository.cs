d56ee735c5a394c2aafcbb546789d11d247a2601
unknown <orkhansab@code.edu.az>
2024-06-08 19:57:06 +0400

    LikeRepository Created

---
+++ EndProject.Data/Repositories/LikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+
+namespace EndProject.Data.Repositories;
+
+public class LikeRepository : GenericRepository<FriendShip>, IFriendShipRepository
+{
+    public LikeRepository(AppDbContext context) : base(context)
+    {
+    }
+}
