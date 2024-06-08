ce269f5a3fc54688d90ac8e99856417459b7dd66
unknown <orkhansab@code.edu.az>
2024-06-08 13:27:18 +0400

    ConversationRepository And MesageRepository Created

---
+++ EndProject.Data/Repositories/ConversationRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+
+namespace EndProject.Data.Repositories;
+
+public class ConversationRepository : GenericRepository<Conversation>, IConversationRepository
+{
+    public ConversationRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ EndProject.Data/Repositories/MessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+
+namespace EndProject.Data.Repositories;
+
+public class MessageRepository : GenericRepository<Message>, IMessageRepository
+{
+    public MessageRepository(AppDbContext context) : base(context)
+    {
+    }
+}
