fc135d1a6d6740a339e9465e284a2149b081becf
unknown <orkhansab@code.edu.az>
2024-06-08 13:24:51 +0400

    CommentRepository Created

---
+++ EndProject.Data/Repositories/CommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+
+namespace EndProject.Data.Repositories;
+
+public class CommentRepository : GenericRepository<Comment>, ICommentRepository
+{
+    public CommentRepository(AppDbContext context) : base(context)
+    {
+    }
+}
