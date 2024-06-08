8f6f576eac24463dea6442d26936dc057ea30995
unknown <orkhansab@code.edu.az>
2024-06-08 13:19:29 +0400

    GenericRepository Created

---
+++ EndProject.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,71 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EndProject.Data.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+{
+    private readonly AppDbContext _context;
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public DbSet<TEntity> Table => _context.Set<TEntity>();
+    
+    public async Task InsertAsync(TEntity entity)
+    {
+        await Table.AddAsync(entity);
+    }
+
+    public async Task<int> CommitAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> expression = null, params string[] includes)
+    {
+        var query = Table.AsQueryable();
+        if (includes is not null)
+        {
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+        return expression is not null
+            ? await query.Where(expression).ToListAsync()
+            : await query.ToListAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        return await Table.FindAsync(id);
+    }
+
+    public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> expression = null, params string[] includes)
+    {
+        var query = Table.AsQueryable();
+        if (includes is not null)
+        {
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+        return expression is not null
+            ? await query.Where(expression).FirstOrDefaultAsync()
+            : await query.FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> IsExist(Expression<Func<TEntity, bool>> expression)
+    {
+        return await Table.AnyAsync(expression);
+    }
+    public void Delete(TEntity entity)
+    {
+        Table.Remove(entity);
+    }
+}
