f33b2da91b5efb18cae10f5663dd095b67b871dd
unknown <orkhansab@code.edu.az>
2024-06-08 19:54:57 +0400

    FriendShipRepository Created

---
+++ EndProject.Data/Repositories/FriendShipRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Core.Entities;
+using EndProject.Core.Repositories;
+using EndProject.Data.Contexts;
+
+namespace EndProject.Data.Repositories;
+
+public class FriendShipRepository : GenericRepository<FriendShip>, IFriendShipRepository
+{
+    public FriendShipRepository(AppDbContext context) : base(context)
+    {
+    }
+}
