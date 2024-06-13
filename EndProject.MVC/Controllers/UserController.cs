b3aa3d27ecc2bfaa646eb43266e9580339b8813b
unknown <orkhansab@code.edu.az>
2024-06-13 23:09:37 +0400

    MVC Project Created

---
+++ EndProject.MVC/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using EndProject.MVC.Models;
+using EndProject.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndProject.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        Uri baseAdress = new Uri("https://localhost:7032/api");
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            return;
+        }
+    }
+}
---
+++ EndProject.MVC/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EndProject.MVC.ViewModels;
+
+public class LoginViewModel
+{
+    [DataType(DataType.Text)]
+    [StringLength(50)]
+    public string UserName { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
